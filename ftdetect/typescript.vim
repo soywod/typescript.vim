1150aba6311842ade55967d0159e3af80b94a2e9
Clément DOUIN <clement.douin@gmail.com>
2018-06-22 17:17:13 +0200

    init repo mixing vim-javascript and vim-jsx

---
+++ ftdetect/typescript.vim
@@ -0,0 +1,3 @@
+au BufNewFile,BufRead *.ts setf typescript
+au BufNewFile,BufRead *.tsx setf typescript.tsx
+
---
+++ test.tsx
@@ -0,0 +1,32 @@
+interface Lolol {
+  test: boolean
+  toto: string
+  lol: Array<Toto>
+}
+
+const toto = {
+  test: "lol",
+  toto: () => "oeuo",
+}
+
+class Toto extends Lol {
+  toto = 34
+  private toto: string = 34
+
+  render() {
+    return (
+      <Toto test="lol" o={true}>
+        <Test
+          test="lol"
+          toto={() => this.coucou()}
+        >
+          {this.toto()}
+          <Toul
+            teto={true}
+          />
+        </Test>
+      </Toto>
+    )
+  }
+}
+
